7008f1525e180419c2c6d6a34b45560ad9106728
Sebastia <calderonsebastian211@gmail.com>
2025-07-09 09:41:25 -0500

    feat: habilitar admin del proyecto (v0.2)

--- taller/taller/empresa/admin.py
+++ taller/taller/empresa/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Edificio, Departamento
 
 # Register your models here.
+class EdificioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'ciudad', 'tipo')
+    search_fields = ('nombre', 'direccion', 'ciudad', 'tipo')
+    
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo', 'costo_departamento', 'numero_cuartos', 'edificio')
+    search_fields = ('nombre_completo', 'costo_departamento', 'numero_cuartos')
+    list_filter = ('edificio',)
+    
+admin.site.register(Edificio, EdificioAdmin)
+admin.site.register(Departamento, DepartamentoAdmin)
